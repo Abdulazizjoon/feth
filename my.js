b3b171b3240edeeb6f6da6ac920989286bfa6b9a
abdulaziz <abdulaziz@gmail.com>
2024-07-04 19:16:43 +0500

    .

--- my.js
+++ my.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded',function() {
                 }
             })
         .then(function (data) {
-            wraper.style.display='block'
+            // wraper.style.display='block'
             data.forEach((user) => {
               let card = createCard(user);
               wraper.innerHTML += card;
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded',function() {
         //   console.log(data);
         })
         .catch(function (err) {
-            wraper.style.display='block'
+            // wraper.style.display='block'
           console.log(err);
         })
         
